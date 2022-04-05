45d2e0ad1e456edc8525880d8de24e1ebfe2f51a
Pierre Torres <kolystudio@outlook.com>
2022-04-06 04:00:12 +0700

    first commit

--- components/config.js
+++ components/config.js
@@ -5,7 +5,7 @@ export const site = {
   fb_pixel: "496114882168181",
   ga_pixel: "G-PX8B89E9G",
   checkoutx: "https://secure.cincar.co/f/Y9NHD36G/b/",
-  offre: "WINTER SALES: ENJOY 50% OFF & FREE SHIPPING",
+  offre: "WINTER SALES: ENJOY 50% OFF & FREE SHIPPING UNTIL 07 APRIL",
 };
 
 export const produit = {
