714449bedda08dc2df4512240f6e740210c2a00a
Pierre Torres <kolystudio@outlook.com>
2022-04-06 18:34:11 +0700

    first commit

--- components/Details.js
+++ components/Details.js
@@ -90,7 +90,7 @@ console.log(link);
         </article>
 
         <article className='flex items-center space-x-1 py-2'>
-          <span className='text-xl font-semibold text-primary'>
+          <span className='text-xl font-semibold text-[#f58523]'>
             {quantity == 1 ? (
               <h3>£{produit.price.toFixed(2)}</h3>
             ) : quantity == 2 ? (
@@ -258,7 +258,7 @@ console.log(link);
 
             <div className='flex flex-col'>
               <span className={` font-bold ${
-              quantity == 1 ? "border-black  text-primary" : "text-primary"}`}>£{produit.price.toFixed(2)}</span>
+              quantity == 1 ? "border-black  text-[#f58523]" : "text-[#f58523]"}`}>£{produit.price.toFixed(2)}</span>
               <span className='line-through font-bold text-gray-500'>£{(produit.price * 2).toFixed(2)}</span>
               </div>
              </div>
@@ -293,7 +293,7 @@ console.log(link);
              <div className='font-semibold w-full text-left pl-3'>1 x CapsLock™ + 2nd 50% OFF <br></br>= £{(((produit.price * 1.5) ) / 2).toFixed(2)} each</div>
             <div className='flex flex-col'>
               <span className={` font-bold ${
-              quantity == 2 ? " text-primary" : "text-primary"}`}>£{(produit.price * 1.5).toFixed(2)}</span>
+              quantity == 2 ? " text-[#f58523]" : "text-[#f58523]"}`}>£{(produit.price * 1.5).toFixed(2)}</span>
               <span className='line-through font-bold text-gray-500'>£{(produit.price * 2 * 2).toFixed(2)}</span>
               </div>
               <div className='absolute bottom-0 left-[35px] text-sm font-medium'></div>
@@ -329,7 +329,7 @@ console.log(link);
 
             <div className='flex flex-col'>
               <span className={` font-bold ${
-              quantity == 3 ? "text-primary" : "text-primary"}`}>£{(produit.price * 2).toFixed(2)}</span>
+              quantity == 3 ? "text-[#f58523]" : "text-[#f58523]"}`}>£{(produit.price * 2).toFixed(2)}</span>
               <span className='line-through font-bold text-gray-500'> £{(produit.price * 2 * 3).toFixed(2)}</span>
               </div>
 
@@ -368,7 +368,7 @@ console.log(link);
           </button>
         </section>
 
-        <section className='mt-5 flex lg:mx-auto lg:mt-2 md:mt-2 justify-center space-x-2 rounded-lg bg-[#eef2fd]   p-1 py-2 text-sm md:mx-auto lg:m-0 md:w-3/5 lg:w-[80%] tracking-wider'>
+        {/* <section className='mt-5 flex lg:mx-auto lg:mt-2 md:mt-2 justify-center space-x-2 rounded-lg bg-[#eef2fd]   p-1 py-2 text-sm md:mx-auto lg:m-0 md:w-3/5 lg:w-[80%] tracking-wider'>
         
         <h3 className="text-center">
            You came from <strong className="text-[#1b74e4]">Facebook </strong> ? Congrats. You win a <strong className="text-[#1b74e4]">special {quantity == 1 ? (
@@ -379,7 +379,7 @@ console.log(link);
               <span>68% </span>
             )} OFF discount</strong>  until <strong className="text-[#1b74e4]">07 April</strong>
         </h3>
-      </section>
+      </section> */}
 
       
 
@@ -395,7 +395,7 @@ console.log(link);
             
           </span>
         </article></div>
-        <div>I bought it for my motorcycle because I was robbed when it was parked on the street. It&apos;s a very nice device, well made, easy to install, my stress is out now.</div>
+        <div>I bought it for my motorcycle because I was robbed when it was parked on the street. It&apos;s a very nice solution, well made, easy to install, my stress is out now.</div>
       </section>
 
         
