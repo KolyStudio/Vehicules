aa13484b8fc5957b767535aebdd43fdd46a33dcd
Pierre Torres <kolystudio@outlook.com>
2022-04-06 14:14:25 +0700

    first commit

--- components/Announcement.js
+++ components/Announcement.js
@@ -2,8 +2,12 @@ import { site } from "./config";
 
 export default function Announcement() {
   return (
-    <section className=' bg-primary  py-2 text-center text-sm font-bold text-white tracking-wider'>
-      {site.offre}
+    <section className=' bg-primary  py-2 text-center text-sm font-bold text-white tracking-wider flex justify-center space-x-2 px-1'>
+    < img
+                alt='produit.price'
+                src='icons/checktest2.svg'
+                className='w-[14px] lg:block hidden'
+              /><div>{site.offre}</div> 
     </section>
   );
 }
