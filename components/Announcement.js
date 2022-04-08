ea0ce3a318c03865cbbf1e69bcba0293c14745fc
Pierre Torres <kolystudio@outlook.com>
2022-04-08 21:04:41 +0700

    first commit

--- components/Announcement.js
+++ components/Announcement.js
@@ -11,13 +11,9 @@ export default function Announcement() {
 
   return (
     <section className=' bg-[#eef2fd] relative  uppercase py-2  items-center text-sm font-base text-BLACK tracking-wider flex justify-center space-x-2 px-1'>
-    <img
-                alt='produit.price'
-                src='icons/fb.svg'
-                className='w-[20px] lg:static absolute left-1'
-              />
+    
               
-               <div className="lg:pl-0  text-center" ><strong className="text-[#1b74e4]">FB OFFER:</strong> <span className="font-medium">50% OFF & FREE DELIVERY UNTIL <strong className="text-[#1b74e4]">{date}</strong></span>
+               <div className="lg:pl-0  text-center" ><strong className="text-[#1b74e4]">WINTER SALES:</strong> <span className="font-medium">50% OFF & FREE DELIVERY UNTIL <strong className="text-[#1b74e4]">{date}</strong></span>
               </div>
     </section>
   );
