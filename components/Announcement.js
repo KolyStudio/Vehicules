5f3206c3d71d70033fa916b1722bd4b5f1e64e67
Pierre Torres <kolystudio@outlook.com>
2022-04-08 20:15:58 +0700

    first commit

--- components/Announcement.js
+++ components/Announcement.js
@@ -1,6 +1,14 @@
 import { site } from "./config";
+import * as dayjs from 'dayjs'
 
 export default function Announcement() {
+
+  const date = dayjs().format('DD MMMM');
+  console.log(date);
+
+  const time = new Date();
+
+
   return (
     <section className=' bg-[#eef2fd] relative   py-2  items-center text-sm font-base text-BLACK tracking-wider flex justify-center space-x-2 px-1'>
     <img
@@ -9,7 +17,7 @@ export default function Announcement() {
                 className='w-[20px] lg:static absolute left-1'
               />
               
-               <div className="lg:pl-0  text-center" ><strong className="text-[#1b74e4]">FB OFFER:</strong> <span className="font-medium">50% OFF & FREE DELIVERY UNTIL <strong className="text-[#1b74e4]">08 APRIL</strong></span>
+               <div className="lg:pl-0  text-center" ><strong className="text-[#1b74e4]">FB OFFER:</strong> <span className="font-medium">50% OFF & FREE DELIVERY UNTIL <strong className="text-[#1b74e4]">{date}</strong></span>
               </div>
     </section>
   );
