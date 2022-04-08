a8401071bb0835dbc5f7ef5f8ebd0df3e70fe197
Pierre Torres <kolystudio@outlook.com>
2022-04-08 15:40:07 +0700

    first commit

--- components/Announcement.js
+++ components/Announcement.js
@@ -2,9 +2,9 @@ import { site } from "./config";
 
 export default function Announcement() {
   return (
-    <section className=' bg-black relative text-center py-2  items-center text-sm  text-white tracking-wider flex justify-center space-x-2 '>
+    <section className=' bg-black relative font-medium text-center py-2  items-center text-sm  text-white tracking-wider flex justify-center space-x-2 '>
   
-  <b> ENJOY FREE SHIPPING TO THE UNITED KINGDOM </b>
+ENJOY FREE SHIPPING TO THE UNITED KINGDOM 
     </section>
   );
 }
